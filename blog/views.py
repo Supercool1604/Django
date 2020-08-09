3991db62c5c637f87e316455a62b071743cf8c66
Supercool1604 <kabirjuneja007@gmail.com>
2020-08-09 22:28:10 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+def index(request):
+	return render(request, 'blog/index.html')
+
+
+
+def business(request):
+	context = {
+	'posts' : Post.objects.all()
+	}
+	return render(request, 'blog/business.html', context)
